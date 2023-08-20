34380153d2b4330c5b0c378b1d88b9081cb2898f
michael-russo <michael.russo@zinniahealth.com>
2023-08-20 16:33:26 -0700

    initial chess board creation

---
+++ client/src/app/page.client.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Button from '@mui/material/Button';
+
+export default function HomeClient() {
+  return (
+    <>
+      <div>Hello, Welcome to the Chess Trainer</div>
+
+      <Button variant="outlined" color="success">
+        Select one of the following training options
+      </Button>
+    </>
+  );
+}
